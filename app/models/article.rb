77664affab3cec3afe7504c49bf6328a99b0134b
mike <mikeover719@gmail.com>
2016-03-15 22:56:49 -0400

    Add user/articles association, add devise helpers to rspec (login_as)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
 
+  belongs_to :user
+
   validates :title, presence: true
   validates :body, presence: true
 
--- spec/features/editing_article_spec.rb
+++ spec/features/editing_article_spec.rb
@@ -3,8 +3,11 @@ require "rails_helper"
 RSpec.feature "Editing an Article" do
   
   before do
+    john = User.create(email: "john@example.com", password: "password")
+    login_as john
     @article = Article.create(title: "First article",
-                              body: "Body of first article")
+                              body: "Body of first article",
+                              user: john)
   end
 
   scenario "A user updates an article" do
