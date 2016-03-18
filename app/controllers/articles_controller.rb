2e0717a88580e1e0a8b72d8937c632b342b908f5
mike <mikeover719@gmail.com>
2016-03-17 21:51:42 -0400

    Restrict access with articles controller and make macros file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,15 +23,24 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    if @article.user != current_user
+      flash[:danger] = "You can only edit your own articles."
+      redirect_to root_path
+    end
   end
   
   def update
-    if @article.update(article_params)
-      flash[:success] = "Article has been updated"
-      redirect_to @article
+    if @article.user != current_user
+      flash[:danger] = "You can only edit your own articles."
+      redirect_to root_path
     else
-      flash.now[:danger] = "Article has not been updated"
-      render :edit and return
+      if @article.update(article_params)
+        flash[:success] = "Article has been updated"
+        redirect_to @article
+      else
+        flash.now[:danger] = "Article has not been updated"
+        render :edit and return
+      end
     end
   end
 
